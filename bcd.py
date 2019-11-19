57a25df8d7748852c8cb7d55c4465487212a7fb6
Loriano Storchi <loriano@storchi.org>
2019-11-19 15:06:57 +0100

    fixed

--- bcd.py
+++ bcd.py
@@ -5,41 +5,38 @@ from matplotlib.patches import Rectangle
 # Create figure and axes
 fig, ax = plt.subplots(1)
 
-color = {0: "r", 1: "g", 2: "b", 3: "b" }
-x = 0
-y = 0
+color = {0: "r", 1: "g", 2: "b", 3: "y" }
 rank = 0
 size = 32
 dim = 1560
 numprocs = 4
-
 dix = size
 diy = size
 numof = int((dim/size) + 0.5) + 1
 
 startrank = 0
-x = 0
+y = dim-32
 for i in range(0,numof):
-    y = 0
+    x = 0
+    rank = startrank
+    maxproc = startrank + (numprocs/2)
 
     for j in range(0,numof):
-      rect = Rectangle((y,x),diy,dix,
+      rect = Rectangle((x,y),dix,dix,
               linewidth=0,edgecolor=color[rank],
               facecolor=color[rank])
       ax.add_patch(rect)
 
       rank += 1
-      if (rank >= (numprocs/2)):
+      if (rank >= maxproc):
         rank = startrank
-      y += 32
-
-    break
+      x += 32
 
     startrank += (numprocs/2)
     if (startrank >= numprocs):
         startrank = 0
 
-    x += 32
+    y -= 32
 
 
 ax.set(xlim=(0, dim), ylim=(0, dim))
